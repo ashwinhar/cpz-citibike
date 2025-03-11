86dcd00af84a54b036db04c79f2d5dae37274034
Ashwin Hareesh <ashwinhar@gmail.com>
2025-03-11 16:17:38 -0400

    Tests for get_existing_citibike_files

--- tests/extract/test_extract.py
+++ tests/extract/test_extract.py
@@ -1,6 +1,9 @@
 """Tests for src.src_components.data_extraction.extract.py"""
 
-from src_components.data_extraction.extract import find_files_of_interest
+from src_components.data_extraction.extract import (
+    find_files_of_interest,
+    get_existing_citibike_files,
+)
 
 
 def test_find_files_of_interest(sample_zip_links, re_pattern):
@@ -78,4 +81,66 @@ def test_find_files_of_interest_with_invalid_pattern(sample_zip_links, re_patter
         re_pattern.get("prefix_bound"),
         year_floor,
     )
+    assert result == expected_files
+
+
+def test_get_existing_citibike_files_with_existing_files(tmp_path):
+    """
+    Test get_existing_citibike_files with existing .zip files in the folder
+    """
+    # Create temporary files
+    file1 = tmp_path / "202101-citibike-tripdata.zip"
+    file2 = tmp_path / "202102-citibike-tripdata.zip"
+    file1.touch()
+    file2.touch()
+
+    expected_files = ["202101-citibike-tripdata.zip", "202102-citibike-tripdata.zip"]
+
+    result = get_existing_citibike_files(folder_path=tmp_path)
+
+    assert sorted(result) == sorted(expected_files)
+
+
+def test_get_existing_citibike_files_with_no_files(tmp_path):
+    """
+    Test get_existing_citibike_files with no .zip files in the folder
+    """
+    expected_files = []
+
+    result = get_existing_citibike_files(folder_path=tmp_path)
+
+    assert result == expected_files
+
+
+def test_get_existing_citibike_files_with_non_zip_files(tmp_path):
+    """
+    Test get_existing_citibike_files with non .zip files in the folder
+    """
+    # Create temporary files
+    file1 = tmp_path / "202101-citibike-tripdata.txt"
+    file2 = tmp_path / "202102-citibike-tripdata.csv"
+    file1.touch()
+    file2.touch()
+
+    expected_files = []
+
+    result = get_existing_citibike_files(folder_path=tmp_path)
+
+    assert result == expected_files
+
+
+def test_get_existing_citibike_files_with_mixed_files(tmp_path):
+    """
+    Test get_existing_citibike_files with mixed .zip and non .zip files in the folder
+    """
+    # Create temporary files
+    file1 = tmp_path / "202101-citibike-tripdata.zip"
+    file2 = tmp_path / "202102-citibike-tripdata.txt"
+    file1.touch()
+    file2.touch()
+
+    expected_files = ["202101-citibike-tripdata.zip"]
+
+    result = get_existing_citibike_files(folder_path=tmp_path)
+
     assert result == expected_files
