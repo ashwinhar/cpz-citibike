e1eacc594eccfe81b5c788058d051804bfc0774a
Ashwin Hareesh <ashwinhar@gmail.com>
2025-03-11 09:13:31 -0400

    Refactor constants

--- src/src_constants/extract_constants.py
+++ src/src_constants/extract_constants.py
@@ -3,9 +3,9 @@
 import os
 
 CITIBIKE_URL_PREFIX = "https://s3.amazonaws.com/tripdata/"
+CITIBIKE_URL = CITIBIKE_URL_PREFIX + "index.html"
 RAW_DATA_FOLDER = os.path.join(os.path.abspath("../data"), "raw_citibike_data")
 RE_PATTERN = (
-    "https://s3\.amazonaws\.com/tripdata/\d{4,6}-citibike-tripdata(?:\.csv)?\.zip"
+    r"https://s3\.amazonaws\.com/tripdata/\d{4,6}-citibike-tripdata(?:\.csv)?\.zip"
 )
 YEAR_FLOOR = 2020
-CITIBIKE_URL = CITIBIKE_URL_PREFIX + "index.html"
