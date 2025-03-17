330c2c8a6ff86413318f1c98bea5a58ebaaaa830
Ashwin Hareesh <ashwinhar@gmail.com>
2025-03-17 14:20:39 -0400

    First dvt run model

---
+++ src/src_components/data_transform/models/staging/2025.sql
@@ -0,0 +1,3 @@
+select * from staging_2025."202501_citibike_tripdata_1" 
+UNION ALL select * from staging_2025."202501_citibike_tripdata_2" 
+UNION ALL select * from staging_2025."202501_citibike_tripdata_3"
--- src/src_constants/extract_constants.py
+++ src/src_constants/extract_constants.py
@@ -4,7 +4,9 @@ import os
 
 CITIBIKE_URL_PREFIX = "https://s3.amazonaws.com/tripdata/"
 CITIBIKE_URL = CITIBIKE_URL_PREFIX + "index.html"
-RAW_DATA_FOLDER = os.path.join(os.path.abspath("../data"), "raw_citibike_data")
+PARENT_DIR = os.path.abspath("/Users/ashwin/Desktop/cpz_citibike/")
+RAW_DATA_FOLDER = os.path.join(PARENT_DIR, "data/raw_citibike_data")
+# TODO: Change this to a relative path
 RE_PATTERN = (
     r"https://s3\.amazonaws\.com/tripdata/\d{4,6}-citibike-tripdata(?:\.csv)?\.zip"
 )
