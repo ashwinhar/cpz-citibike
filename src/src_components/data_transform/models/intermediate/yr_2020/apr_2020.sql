d245d708ca7725ca4c65a182847a1b210fe03035
Ashwin Hareesh <ashwinhar@gmail.com>
2025-03-19 12:59:03 -0400

    models jan-apr 2020

---
+++ src/src_components/data_transform/models/intermediate/yr_2020/apr_2020.sql
@@ -0,0 +1,3 @@
+select * from 
+{{ source('staging_2020', '202004_citibike_tripdata_1') }} 
+
---
+++ src/src_components/data_transform/models/intermediate/yr_2020/jan_2020.sql
@@ -0,0 +1,4 @@
+select * from 
+{{ source('staging_2020', '202001_citibike_tripdata_1') }} 
+UNION ALL 
+select * from {{ source('staging_2020', '202001_citibike_tripdata_2') }}
