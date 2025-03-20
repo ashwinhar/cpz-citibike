d8174442c6b6b1eca5b619395a87f9e0a37e4758
Ashwin Hareesh <ashwinhar@gmail.com>
2025-03-20 11:14:28 -0400

    fact table

---
+++ src/src_components/data_transform/models/marts/fct_citibike_ridership.sql
@@ -0,0 +1,11 @@
+select * from {{ref ("yr_2020")}}
+UNION ALL
+select * from {{ref ("yr_2021")}}
+UNION ALL
+select * from {{ref ("yr_2022")}}
+UNION ALL
+select * from {{ref ("yr_2023")}}
+UNION ALL
+select * from {{ref ("yr_2024")}}
+UNION ALL
+select * from {{ref ("yr_2025")}}
