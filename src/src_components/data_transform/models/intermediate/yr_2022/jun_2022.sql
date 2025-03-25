95f0f822149be59bb3f1940f3154c954d1e7d389
Ashwin Hareesh <ashwinhar@gmail.com>
2025-03-25 09:28:19 -0400

    All files 2022

--- src/src_components/data_transform/models/intermediate/yr_2022/jun_2022.sql
+++ src/src_components/data_transform/models/intermediate/yr_2022/jun_2022.sql
@@ -1,8 +1,25 @@
-select * from 
-{{ source('staging_2022', '202206_citibike_tripdata_1') }} 
-UNION ALL 
-select * from {{ source('staging_2022', '202206_citibike_tripdata_2') }}
-UNION ALL 
-select * from {{ source('staging_2022', '202206_citibike_tripdata_3') }}
-UNION ALL 
-select * from {{ source('staging_2022', '202206_citibike_tripdata_4') }}
+with cte as (
+    select * from 
+    {{ source('staging_2022', '202206_citibike_tripdata_1') }} 
+    UNION ALL 
+    select * from {{ source('staging_2022', '202206_citibike_tripdata_2') }}
+    UNION ALL 
+    select * from {{ source('staging_2022', '202206_citibike_tripdata_3') }}
+    UNION ALL 
+    select * from {{ source('staging_2022', '202206_citibike_tripdata_4') }}
+)
+select
+    ride_id,
+    rideable_type,
+    {{ format_date('started_at') }} as started_at,
+    {{ format_date('ended_at') }} as ended_at,
+    start_station_name,
+    start_station_id,
+    end_station_name,
+    end_station_id,
+    start_lat,
+    start_lng,
+    end_lat,
+    end_lng, 
+    member_casual
+from cte
