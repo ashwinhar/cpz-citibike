f6585853dc4f26983021a66de47badf90b5a5acc
Ashwin Hareesh <ashwinhar@gmail.com>
2025-03-25 09:35:10 -0400

    Format date for all 2020

--- src/src_components/data_transform/models/intermediate/yr_2020/jul_2020.sql
+++ src/src_components/data_transform/models/intermediate/yr_2020/jul_2020.sql
@@ -1,6 +1,23 @@
-select * from 
-{{ source('staging_2020', '202007_citibike_tripdata_1') }} 
-UNION ALL 
-select * from {{ source('staging_2020', '202007_citibike_tripdata_2') }}
-UNION ALL 
-select * from {{ source('staging_2020', '202007_citibike_tripdata_3') }}
+with cte as (
+    select * from 
+    {{ source('staging_2020', '202007_citibike_tripdata_1') }} 
+    UNION ALL 
+    select * from {{ source('staging_2020', '202007_citibike_tripdata_2') }}
+    UNION ALL 
+    select * from {{ source('staging_2020', '202007_citibike_tripdata_3') }}
+)
+select
+    ride_id,
+    rideable_type,
+    {{ format_date('started_at') }} as started_at,
+    {{ format_date('ended_at') }} as ended_at,
+    start_station_name,
+    start_station_id,
+    end_station_name,
+    end_station_id,
+    start_lat,
+    start_lng,
+    end_lat,
+    end_lng, 
+    member_casual
+from cte
