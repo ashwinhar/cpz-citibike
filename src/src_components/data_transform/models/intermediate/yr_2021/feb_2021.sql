18db7990c36a1fc9476574a4e72d167e40873cbb
Ashwin Hareesh <ashwinhar@gmail.com>
2025-03-25 09:32:20 -0400

    formatted date for all 2021

--- src/src_components/data_transform/models/intermediate/yr_2021/feb_2021.sql
+++ src/src_components/data_transform/models/intermediate/yr_2021/feb_2021.sql
@@ -1,2 +1,19 @@
-select * from 
-{{ source('staging_2021', '202102_citibike_tripdata_1') }} 
+with cte as (
+    select * from 
+    {{ source('staging_2021', '202102_citibike_tripdata_1') }} 
+)
+select
+    ride_id,
+    rideable_type,
+    {{ format_date('started_at') }} as started_at,
+    {{ format_date('ended_at') }} as ended_at,
+    start_station_name,
+    start_station_id,
+    end_station_name,
+    end_station_id,
+    start_lat,
+    start_lng,
+    end_lat,
+    end_lng, 
+    member_casual
+from cte
