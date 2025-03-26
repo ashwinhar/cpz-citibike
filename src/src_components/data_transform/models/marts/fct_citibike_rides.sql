225a2dc1871786e6b3fe952cc4267ea5cc2c4256
Ashwin Hareesh <ashwinhar@gmail.com>
2025-03-26 11:40:09 -0400

    intermediate to combine years

--- src/src_components/data_transform/models/marts/fct_citibike_rides.sql
+++ src/src_components/data_transform/models/marts/fct_citibike_rides.sql
@@ -1,11 +1,1 @@
-select * from {{ref ("yr_2020")}}
-UNION ALL
-select * from {{ref ("yr_2021")}}
-UNION ALL
-select * from {{ref ("yr_2022")}}
-UNION ALL
-select * from {{ref ("yr_2023")}}
-UNION ALL
-select * from {{ref ("yr_2024")}}
-UNION ALL
-select * from {{ref ("yr_2025")}}
+select * from {{ ref ("combined_years") }}
