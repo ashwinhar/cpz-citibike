6a7ab432212c55272cabaa4eb9d3dcab4aa3c5a9
Ashwin Hareesh <ashwinhar@gmail.com>
2025-03-26 09:17:26 -0400

    bug fix, improper source

--- src/src_components/data_transform/models/intermediate/yr_2022/feb_2022.sql
+++ src/src_components/data_transform/models/intermediate/yr_2022/feb_2022.sql
@@ -3,7 +3,7 @@ with cte as (
     {{ source('staging_2022', '202202_citibike_tripdata_1') }} 
     UNION ALL
     select * from 
-    {{ source('staging_2022', '202202_citibike_tripdata_1') }} 
+    {{ source('staging_2022', '202202_citibike_tripdata_2') }} 
 )
 select
     ride_id,
--- src/src_components/data_transform/models/intermediate/yr_2023/feb_2023.sql
+++ src/src_components/data_transform/models/intermediate/yr_2023/feb_2023.sql
@@ -2,7 +2,7 @@ with cte as (
     select * from {{ source('staging_2023', '202302_citibike_tripdata_1') }} 
     UNION ALL
     select * from 
-    {{ source('staging_2023', '202302_citibike_tripdata_1') }} 
+    {{ source('staging_2023', '202302_citibike_tripdata_2') }} 
 )
 select
     ride_id,
