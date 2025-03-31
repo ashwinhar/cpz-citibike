71d8bc3e4154d96149b9a95a7a2ba674da0b27f0
Ashwin Hareesh <ashwinhar@gmail.com>
2025-03-31 17:18:43 -0400

    formatted with sqlfluff

--- src/src_components/data_transform/models/intermediate/stations/station_in_cpz.sql
+++ src/src_components/data_transform/models/intermediate/stations/station_in_cpz.sql
@@ -4,25 +4,27 @@ with cte as (
         citibike_station_id,
         station_latitude,
         station_longitude
-    from {{ ref('station_geographies')}}
-), 
+    from {{ ref('station_geographies') }}
+),
+
 in_polygon as (
-    select 
+    select
         cte.*,
-        exists (
-            select 1 
-            from utils.cpz_polygons poly
-            where ST_Within(
-                ST_Point(cte.station_longitude, cte.station_latitude),
+        exists(
+            select 1
+            from utils.cpz_polygons as poly
+            where st_within(
+                st_point(cte.station_longitude, cte.station_latitude),
                 poly.geometry
             )
         ) as in_cpz
     from cte
 ),
+
 converted as (
-    select 
-        citibike_station_id, 
-        station_latitude, 
+    select
+        citibike_station_id,
+        station_latitude,
         station_longitude,
         case
             when in_cpz is true then 'inside'
@@ -30,28 +32,34 @@ converted as (
         end as in_cpz
     from in_polygon
 ),
+
 find_spread as (
-    select 
-        citibike_station_id, 
-        in_cpz, 
+    select
+        citibike_station_id,
+        in_cpz,
         count(*) as count_instances
     from converted
-    group by 
-        citibike_station_id, 
-        in_cpz    
+    group by
+        citibike_station_id,
+        in_cpz
 ),
+
 pivoted as (
     pivot find_spread
     on in_cpz in ('inside', 'outside')
-    using coalesce(sum(count_instances),0)
-    group by citibike_station_id 
+    using coalesce(sum(count_instances), 0)
+    group by citibike_station_id
 )
-select 
+
+select
     citibike_station_id,
     case
-        when abs(subtract(inside, outside)) < 100 then False
-        when abs(subtract(inside, outside)) >= 100 and inside > outside then True
-        when abs(subtract(inside, outside)) >= 100 and inside < outside then False
-        else null
-     end as in_cpz
+        when abs(subtract(inside, outside)) < 100 then false
+        when
+            abs(subtract(inside, outside)) >= 100 and inside > outside
+            then true
+        when
+            abs(subtract(inside, outside)) >= 100 and inside < outside
+            then false
+    end as in_cpz
 from pivoted
