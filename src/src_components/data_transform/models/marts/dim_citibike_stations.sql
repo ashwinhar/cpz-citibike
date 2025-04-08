047e8507f3ca9e3b7cacaa136f2e21cd8dd1aeeb
Ashwin Hareesh <ashwinhar@gmail.com>
2025-04-08 15:10:24 -0400

    New dim_citibike_stations with lat_long mode

--- src/src_components/data_transform/models/marts/dim_citibike_stations.sql
+++ src/src_components/data_transform/models/marts/dim_citibike_stations.sql
@@ -1,8 +1,20 @@
-with selected_name as (
+with new_struct as (
+    select
+        citibike_station_name,
+        citibike_station_id,
+        {
+            'station_latitude': station_latitude,
+            'station_longitude': station_longitude
+        } as lat_long_pair
+    from station_geographies
+),
+
+selected_name as (
     select
         citibike_station_id,
-        first(citibike_station_name) as citibike_station_name
-    from {{ ref('station_geographies') }}
+        first(citibike_station_name) as citibike_station_name,
+        mode(lat_long_pair) as lat_long_pair_mode
+    from new_struct
     group by citibike_station_id
 )
 
