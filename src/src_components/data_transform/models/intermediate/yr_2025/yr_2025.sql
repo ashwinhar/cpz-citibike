536387950f3bd80c0e518ff11ac3c19c9cb6895a
Ashwin Hareesh <ashwinhar@gmail.com>
2025-06-11 08:59:55 -0400

    may upload

--- src/src_components/data_transform/models/intermediate/yr_2025/yr_2025.sql
+++ src/src_components/data_transform/models/intermediate/yr_2025/yr_2025.sql
@@ -1,11 +1,13 @@
 with cte as (
     select * from {{ref ("jan_2025")}}
-    UNION ALL
+    union all
     select * from {{ref ("feb_2025")}}
-    UNION ALL 
+    union all 
     select * from {{ref ("mar_2025")}}
     union all
     select * from {{ref ("apr_2025")}}
+    union all
+    select * from {{ref ("may_2025")}}
 )
 select * from cte
 {{ remove_unwanted_station_ids() }}
